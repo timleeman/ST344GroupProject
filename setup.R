ccd7a2c180693cdc6037ffce84999627b1fbd3b8
timleeman <51028830+timleeman@users.noreply.github.com>
2020-11-23 21:36:02 +0000

    Split set up file into packages and data grabbing

--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
-install.packages("dplyr", "tidyverse", "remotes", "lubridate", "readxl", "rio")
+install.packages("dplyr", "tidyverse", "remotes", "lubridate", "readxl", "rio", "ggpubr")
 remotes::install_github("nset-ornl/wbstats")
 remotes::install_github("joachim-gassen/tidycovid19")
 library(tidyverse)
@@ -8,37 +8,4 @@ library(lubridate)
 library(readxl)
 library(rio)
 library(tidycovid19)
-auravisionData <- read_csv("https://raw.githubusercontent.com/timleeman/ST344GroupProject/main/Data/AuraVisionCleaned.csv")
-countries <- read_csv("https://raw.github.com/timleeman/ST344GroupProject/main/Data/Continents.csv")
-covidData <- read_csv("https://raw.github.com/timleeman/ST344GroupProject/main/Data/TidyCovid19DataSet.csv", guess_max = 10000)
-
-countries <- filter(countries, country %in% covidData$country)
-covidData <- left_join(covidData, countries, by = "country")
-covidData <- mutate(covidData, country = factor(country), continent = factor(continent))
-covidData$country <- as.factor(covidData$country)
-#Getting just the relevant continents
-covidData <- filter(covidData, continent == "Europe" | continent == "North America" | continent == "South America" | country == "Turkey")
-co <- ""
-#Remove countries with excessive amounts of data missing in gcmr_retail_recreation column
-for (co in levels(covidData$country)){
-  #100 can be changed to another threshold
-  if(sum(is.na(filter(covidData, country == co)$gcmr_retail_recreation)) >= 100){
-    covidData <- filter(covidData, country != co)
-  }
-}
-
-#Remove countries with excessive amounts of data missing in gcmr_grocery_pharmacy column
-for (co in levels(covidData$country)){
-  #100 can be changed to another threshold
-  if(sum(is.na(filter(covidData, country == co)$gcmr_grocery_pharmacy)) >= 100){
-    covidData <- filter(covidData, country != co)
-  }
-}
-#Getting rid of countries with populations less than 1,000,000
-covidData <- filter(covidData, population > 1000000)
-covidData <- droplevels(covidData)
-chosenCountries <- levels(covidData$country)
-covidData <- read_csv("https://raw.github.com/timleeman/ST344GroupProject/main/Data/TidyCovid19DataSet.csv", guess_max = 10000)
-countries <- filter(countries, country %in% covidData$country)
-covidData <- left_join(covidData, countries, by = "country")
-covidData <- mutate(covidData, country = factor(country), continent = factor(continent))
+library(ggbpubr)
