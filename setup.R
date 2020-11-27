f8aeee6ad022f84c874d1df4c1d61be9738c9869
timleeman <51028830+timleeman@users.noreply.github.com>
2020-11-27 16:32:14 +0000

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,4 +1,5 @@
-install.packages("dplyr", "tidyverse", "remotes", "lubridate", "readxl", "rio", "ggpubr")
+install.packages("dplyr", "zoo", "remotes", "lubridate", "readxl", "rio", "ggpubr")
+install.packages("tidyverse")
 remotes::install_github("nset-ornl/wbstats")
 remotes::install_github("joachim-gassen/tidycovid19")
 library(tidyverse)
